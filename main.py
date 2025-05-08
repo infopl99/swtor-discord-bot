9f76b22692facf3b1e09f9190af61d9c23875dbd
infopl99 <infopl99@gmail.com>
2025-05-08 07:57:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from discord import app_commands
 import sqlite3
 import os
 from dotenv import load_dotenv
+import logging
+
+logger = logging.getLogger("swtor_bot")
+logging.basicConfig(level=logging.INFO)
 
 # Chargement du token depuis une variable d'environnement ou un fichier .env
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -21,19 +25,19 @@ async def setup_hook():
     for filename in os.listdir("./commands"):
         if filename.endswith(".py"):
             await bot.load_extension(f"commands.{filename[:-3]}")
-            print(f"🔁 Extension chargée : {filename}")
+            logger.info(f"🔁 Extension chargée : {filename}")
 
 # Événement on_ready + synchronisation slash
 @bot.event
 async def on_ready():
-    print(f"Connecté en tant que {bot.user}")
+    logger.info(f"Connecté en tant que {bot.user}")
     try:
         await bot.tree.clear_commands(guild=None)  # vide les commandes globales
         synced = await bot.tree.sync(guild=discord.Object(id=GUILD_ID))  # pour le serveur actuel
         await bot.tree.sync()  # pour synchroniser globalement
-        print("Commandes synchronisées.")
-        print(f"{len(synced)} commandes synchronisées avec succès.")
+        logger.info("Commandes synchronisées.")
+        logger.info(f"{len(synced)} commandes synchronisées avec succès.")
     except Exception as e:
-        print(f"Erreur de synchronisation des commandes : {e}")
+        logger.info(f"Erreur de synchronisation des commandes : {e}")
 
 bot.run(TOKEN)
